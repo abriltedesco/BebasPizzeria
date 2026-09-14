557089e2a62af23de177701426de78c35d08abcf
Eugeles <eufrales@gmail.com>
2026-09-14 17:02:11 -0300

    movimiento del cliente al centro de la pantalla

---
+++ bebas-pizzeria/scripts/movimiento_cliente.gd
@@ -0,0 +1,17 @@
+extends Node
+@onready var sprite = $"../Icon"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	movimiento()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func movimiento():
+	var tween = create_tween()
+	var posicionFinal = Vector2(sprite.position.x - 500, sprite.position.y)
+	tween.tween_property(sprite, "position", posicionFinal, 2) 
+	await tween.finished
